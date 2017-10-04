69dd4f309273f2342c73b57974bba213246fad71
Andreas Bielawski <brawl98@outlook.com>
2017-10-04 23:49:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+RiiConnect24 IOS Patcher WiiDatabase Edition
+============================================
+
+An improved version of the [RiiConnect24 IOS Patcher](https://github.com/RiiConnect24/IOS-Patcher) by KcrPL and Larsenv. It *works*, but the code is a mess imho. May improve it someday.
+
+## Main changes:
+* Rewrote from scratch
+* Checks downloaded IOS and patched files against their checksums
+* Works from Windows XP to 10
+* Faster without unnecessary questions
+
+## Todo
+* [ ] Better download and patching code without [lotsa spaghetti](https://abload.de/img/hqdefaultj8s9a.jpg)
+* [ ] Check MD5 of patched files before downloading them
+* [ ] Speaking of MD5, switch to a more secure algorithm
+
+## Screenshots
+![Start](https://abload.de/img/virtualbox_windowsxp_63s3c.png)
+![Working](https://abload.de/img/virtualbox_windowsxp_iesxh.png)
---
+++ patch.bat
@@ -0,0 +1,184 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+:top
+CLS
+COLOR 1F
+set currentversion=v1.0
+set header=echo			RiiConnect24 IOS Patcher %currentversion% by WiiDatabase.de
+mode con cols=85 lines=30
+TITLE RiiConnect24 IOS Patcher %currentversion%
+
+:check
+if not exist Support\IOS31.diff goto:missing
+if not exist Support\IOS80.diff goto:missing
+if not exist Support\nusd.exe goto:missing
+if not exist Support\sfk.exe goto:missing
+if not exist Support\xdelta3.exe goto:missing
+goto:start
+
+:missing
+CLS
+%header%
+echo.
+echo.
+echo		ERROR: One or more support files are missing.
+echo		Please check the download and redownload
+echo		if necessary.
+echo.
+echo		Press any key to close the patcher.
+pause >NUL
+exit
+
+:start
+CLS
+%header%
+echo.
+if /i "%false%" EQU "1" Support\sfk echo -spat \x20 \x20 \x20 \x20 \x20 \x20 [Red] Not a valid input, please try again.
+set false=
+set islatest=
+echo.
+echo		Welcome to the RiiConnect24 IOS Patcher!
+echo.
+Support\sfk echo -spat \x20 \x20 \x20 \x20 \x20 \x20 [Yellow] PLEASE NOTE: This patcher will ONLY work on the Wii!
+Support\sfk echo -spat \x20 \x20 \x20 \x20 \x20 \x20 [Yellow] Do not install these IOS on a Wii U!
+echo.
+echo		Do you have System Menu 4.3 installed and want to use Wii Mail?
+echo		In this case, IOS80 will also be patched.
+echo.
+echo.
+echo		[Y] Yes
+echo		[N] No
+echo		[0] Exit
+echo.
+set /p islatest=	Input:	
+
+if /i "%islatest%" EQU "Y" (set ios31=*) && (set ios80=*) && (goto:downloadios)
+if /i "%islatest%" EQU "N" (set ios31=*) && (set ios80=) && (goto:downloadios)
+if /i "%islatest%" EQU "0" exit
+
+set false=1
+goto:start
+
+:downloadios
+CLS
+%header%
+echo.
+echo.
+:setvariables
+set attempt=1
+if /i "%ios31%" EQU "*" goto:ios31
+if /i "%ios80%" EQU "*" goto:ios80
+goto:alldownloadsfinished
+
+
+:ios31
+set name=IOS31
+set namedl=IOS31 Latest Version
+set titleid=000000010000001F
+set titleversion=3608
+set md5=7908ce72ed970b9610d817dd71807a84
+set patched_md5=a4dfbb1c46fdd815eefbd9c463f6ed63
+set variable=ios31
+goto:startdownload
+
+:ios80
+set name=IOS80
+set namedl=IOS80 v6944
+set titleid=0000000100000050
+set titleversion=6944
+set md5=a845fbe6d788f3a5c65ce624096150f4
+set patched_md5=2514cfb7e9c6b566d8db4fe165e711c6
+set variable=ios80
+goto:startdownload
+
+
+:startdownload
+if /i "%attempt%" EQU "3" goto:failed
+if not exist tmp mkdir tmp
+if /i "%attempt%" EQU "1" Support\sfk echo -spat \x20 \x20 [Magenta] Downloading %namedl%...
+if /i "%attempt%" NEQ "1" Support\sfk echo -spat \x20 \x20 [Magenta] Redownloading %namedl%...
+if not exist tmp\%name%.wad goto:download
+set md5check=
+support\sfk md5 -quiet -verify %md5% tmp\%name%.wad
+if errorlevel 1 set md5check=fail
+IF "%md5check%"=="" (set md5check="exists") && (goto:pass)
+
+support\sfk echo -spat \x20 \x20  [Yellow] This file already exists, but it failed the MD5 check.
+support\sfk echo -spat \x20 \x20  [Yellow] The current file will be deleted and the file will be redownloaded.
+echo.
+set /a attempt=%attempt%+1
+del tmp\%name%.wad >NUL
+if exist titles\%titleid% rmdir /s /q titles\%titleid%
+goto:startdownload
+
+:download
+start /min/wait Support\nusd.exe %titleid% %titleversion% packwad
+move /y titles\%titleid%\%titleversion%\%titleid%-NUS-v%titleversion%.wad tmp\%name%.wad >nul
+if exist titles\%titleid% rd /s /q titles\%titleid%
+if exist tmp\%name%.wad goto:checkdownload
+support\sfk echo -spat \x20 \x20  [Yellow] The file is missing, retrying download...
+echo.
+set /a attempt=%attempt%+1
+del tmp\%name%.wad >NUL
+goto:startdownload
+
+:checkdownload
+set md5check=
+support\sfk md5 -quiet -verify %md5% tmp\%name%.wad
+if errorlevel 1 set md5check=fail
+IF "%md5check%"=="" (set md5check=pass) && (goto:pass)
+
+support\sfk echo -spat \x20 \x20  [Yellow] The MD5 check failed.
+support\sfk echo -spat \x20 \x20  [Yellow] The current file will be deleted and the file will be redownloaded.
+echo.
+set /a attempt=%attempt%+1
+del tmp\%name%.wad >NUL
+if exist titles\%titleid% rd /s /q titles\%titleid%
+goto:startdownload
+
+:pass
+if /i %md5check% EQU "exists" (support\sfk echo -spat \x20 \x20  [Green] This file already exists and has passed the MD5 check!) else (support\sfk echo -spat \x20 \x20  [Green] Download finished.)
+echo.
+
+:patching
+Support\sfk echo -spat \x20 \x20 [Magenta] Patching %namedl%...
+if not exist copy-to-device mkdir copy-to-device
+:: Verify MD5, if patched WAD already exists
+set md5check=
+if not exist copy-to-device\%name%-patched.wad (set md5check=notexist) && (goto:miniskip)
+support\sfk md5 -quiet -verify %patched_md5% copy-to-device\%name%-patched.wad
+if errorlevel 1 (set md5check=fail) else (set md5check=pass)
+if /i %md5check% EQU fail (support\sfk echo -spat \x20 \x20  [Yellow] MD5 check for existing file failed, repatching...) && (del copy-to-device\%name%-patched.wad)
+if /i %md5check% EQU pass goto:skippatching
+:miniskip
+start /min/wait Support\xdelta3.exe -d -s tmp\%name%.wad Support\%name%.diff copy-to-device\%name%-patched.wad
+:skippatching
+if not exist copy-to-device\%name%-patched.wad goto:patchfailed
+if /i %md5check% EQU pass (support\sfk echo -spat \x20 \x20  [Green] File is already patched and valid!) else (support\sfk echo -spat \x20 \x20  [Green] Patching done!)
+echo.
+set %variable%=
+goto:setvariables
+
+:patchfailed
+if /i "%attempt%" EQU "1" Support\sfk echo -spat \x20 \x20 [Red] Patching failed, skipping...
+echo.
+set %variable%=
+goto:setvariables
+
+:failed
+echo.
+support\sfk echo -spat \x20 \x20  [Red] This file failed to download multiple times, skipping...
+set %variable%=
+echo.
+goto:setvariables
+
+:alldownloadsfinished
+echo.
+support\sfk echo -spat \x20 \x20  [Green] Everything done!
+echo.
+echo		You can find the patched file(s) in the "copy-to-device" folder.
+echo		Install it/them with a WAD manager.
+echo.
+echo		Press any key to close this patcher.
+pause >NUL
+exit
